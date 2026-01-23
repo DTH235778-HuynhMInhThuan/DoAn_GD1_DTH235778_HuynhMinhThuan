23483f0c3bd6bb15022891b2640a6e06a5444710
DTH235778-HuynhMInhThuan <thuan_dth235778@student.agu.edu.vn>
2026-01-23 12:00:00 +0700

    DoAn_GD1

---
+++ QuanLyNhaTro/Data/HopDong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhaTro.Data
+{
+    public class HopDong
+    {
+        [Key]
+        public int MaHopDong { get; set; }
+
+        public int MaSV { get; set; }
+        public int MaPhong { get; set; }
+
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+
+        public SinhVien SinhVien { get; set; }
+        public Phong Phong { get; set; }
+    }
+}
